f81d78c6ed9e5e4b442541fd0402ba67e0ee2c3b
Chairoel <projectchairul@gmail.com>
2025-02-13 08:20:05 +0700

    use case grouping

--- favorite/src/main/java/com/mascill/githubapps/favorite/FavoriteViewModel.kt
+++ favorite/src/main/java/com/mascill/githubapps/favorite/FavoriteViewModel.kt
@@ -2,7 +2,7 @@ package com.mascill.githubapps.favorite
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
-import com.mascill.githubapps.core.domain.usecase.UserUseCase
+import com.mascill.githubapps.core.domain.usecase.user.UserUseCase
 
 class FavoriteViewModel(userUseCase: UserUseCase) : ViewModel() {
     val favoriteUser = userUseCase.getFavoriteUser().asLiveData()
