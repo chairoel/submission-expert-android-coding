1d197113e8323923afb40e353b342d75ef152aef
Chairoel <projectchairul@gmail.com>
2025-03-02 22:51:48 +0700

    separate favorite detail view model

--- app/src/main/java/com/mascill/githubapps/di/AppModule.kt
+++ app/src/main/java/com/mascill/githubapps/di/AppModule.kt
@@ -7,6 +7,7 @@ import com.mascill.githubapps.core.domain.usecase.user.UserUseCase
 import com.mascill.githubapps.core.ui.viewmodel.SearchViewModel
 import com.mascill.githubapps.core.ui.viewmodel.ThemeViewModel
 import com.mascill.githubapps.detail.DetailViewModel
+import com.mascill.githubapps.detail.FavoriteDetailViewModel
 import com.mascill.githubapps.home.HomeViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
@@ -19,6 +20,7 @@ val useCaseModule = module {
 val viewModelModule = module {
     viewModel { HomeViewModel(get()) }
     viewModel { DetailViewModel(get()) }
+    viewModel { FavoriteDetailViewModel(get()) }
     viewModel { ThemeViewModel(get()) }
     viewModel { SearchViewModel(get()) }
 }
